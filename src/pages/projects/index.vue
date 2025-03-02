4d435d4a40d9507557cb428d9539282111af73f7
Ismat Mahmood <ismatmahmood@dreams.co.uk>
2025-03-02 18:00:09 +0000

    code refactor to move out util queries

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,25 +1,23 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-
-import type { Tables } from '../../../database/types'
-import type { ColumnDef } from '@tanstack/vue-table'
 import { RouterLink } from 'vue-router'
+import { projectsQuery } from '@/utils/supaQueries'
+import type { ColumnDef } from '@tanstack/vue-table'
+import type { Projects } from '@/utils/supaQueries'
 
 usePageStore().pageData.title = 'Projects'
 
-const projects = ref<Tables<'projects'>[] | null>(null)
-async function getProjects() {
-  const { data, error } = await supabase.from('projects').select()
+const projects = ref<Projects | null>(null)
+const getProjects = async () => {
+  const { data, error } = await projectsQuery
 
   if (error) console.log(error)
 
   projects.value = data
-
 }
 
 await getProjects()
 
-const columns: ColumnDef<Tables<'projects'>>[] = [
+const columns: ColumnDef<Projects[0]>[] = [
   {
     accessorKey: 'name',
     header: () => h('div', { class: 'text-left' }, 'Name'),
