9c150f130cbbaa42368783020b90bb25e9fe168f
Ismat Mahmood <ismatmahmood@dreams.co.uk>
2025-03-02 00:13:45 +0000

    adding ts support to supabase in project

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,4 +1,17 @@
 <script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient';
+import { ref } from 'vue';
+import type { Tables } from 'database/types';
+
+const projects =  ref<Tables<'projects'>[] | null>(null)
+
+;(async () => {
+  const {data, error} = await supabase.from('projects').select()
+
+  if (error) console.log(error)
+  projects.value = data
+  console.log('Projects: ', projects.value)
+})()
 
 </script>
 
@@ -6,5 +19,10 @@
     <div>
       <h1>Projects Page</h1>
       <RouterLink to="/">homepage</RouterLink>
+      <ul>
+        <li v-for="project in projects" :key="project.id">
+          {{ project.name }}
+        </li>
+      </ul>
     </div>
 </template>
