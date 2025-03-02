f2b6f3642dd882dd8aa934f0f6c62fcd90bb2704
Ismat Mahmood <ismatmahmood@dreams.co.uk>
2025-03-02 13:02:27 +0000

    set up take in projects and finish setting up table in tasks page, both now done

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -22,7 +22,35 @@ const columns: ColumnDef<Tables<'tasks'>>[] = [
     cell: ({ row }) => {
       return h('div', { class: 'text-left font-medium' }, row.getValue('name'))
     },
-  }
+  },
+  {
+    accessorKey: 'status',
+    header: () => h('div', { class: 'text-left' }, 'Status'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
+    },
+  },
+  {
+    accessorKey: 'due_date',
+    header: () => h('div', { class: 'text-left' }, 'Due Date'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('due_date'))
+    },
+  },
+  {
+    accessorKey: 'project_id',
+    header: () => h('div', { class: 'text-left' }, 'Project'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, row.getValue('project_id'))
+    },
+  },
+  {
+    accessorKey: 'collaborators',
+    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
+    cell: ({ row }) => {
+      return h('div', { class: 'text-left font-medium' }, JSON.stringify(row.getValue('collaborators')))
+    },
+  },
 ]
 </script>
 
